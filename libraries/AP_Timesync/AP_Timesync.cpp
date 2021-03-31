c1325d232087475cdfe7227a214c72a70a227429
MAD-CRAZY-MAN <awake_noh@naver.com>
2021-03-31 15:34:57 +0900

    modified, add, test: send timestamp

--- libraries/AP_Timesync/AP_Timesync.cpp
+++ libraries/AP_Timesync/AP_Timesync.cpp
@@ -16,18 +16,68 @@ AP_Timesync::AP_Timesync()
     _singleton = this;
 }
 
-void AP_Timesync::handle_sync(mavlink_ptp_timesync_t &packet)
+void AP_Timesync::handle_ptp_timesync(GCS_MAVLINK &link, const mavlink_message_t &msg)
 {
-    t1.tv_sec = packet.time_sec;
-    t1.tv_nsec = packet.time_usec;
-    hal.uartA->printf("received sync\r\n");
-    hal.uartA->printf("t1.sec: %d, t1.usec: %d\r\n", t1.tv_sec, t1.tv_nsec);
+    mavlink_ptp_timesync_t packet;
+    mavlink_msg_ptp_timesync_decode(&msg, &packet);
+
+    static uint8_t msg_type = PTP_DEFAULT_STATE;
+    
+    msg_type = packet.msg_type;
+
+    switch(msg_type){
+        case PTP_SYNC: {
+            handle_sync(packet);
+            break;
+        }
+        case PTP_FOLLOW_UP: {
+            handle_follow_up(link, packet);
+            break;
+        }
+        case PTP_DELAY_REQUEST: {
+            handle_delay_response(packet);
+            break;
+        }
+        default:
+            break;
+    }
 }
 
-void AP_Timesync::handle_follow_up(mavlink_ptp_timesync_t &packet)
+void AP_Timesync::handle_sync(mavlink_ptp_timesync_t &packet)
 {
+   // t2.tv_sec = //sync 받은 sec
+    //t2.tv_nsec = //sync 받은 nsec
+   
+}
+
+void AP_Timesync::handle_follow_up(GCS_MAVLINK &link, mavlink_ptp_timesync_t &packet)
+{
+    _request_sending_link = &link;
+ 
     t1.tv_sec = packet.time_sec;
-    t1.tv_nsec = packet.time_usec;
+    t1.tv_nsec = packet.time_nsec;
+    //delay_request.time_sec = //get time sec
+    //delay_request.time_nsec = //get time nsec
+
+    //t3.tv_sec = get time sec;
+    //t3.tv_nsec = get time nsec
+    
+    mavlink_ptp_timesync_t delay_request;
+    
+    delay_request.msg_type = PTP_DELAY_REQUEST;
+    delay_request.seq = 2;
+    //delay_request.time_sec = t3.tv_sec;
+    //delay_request.time_nsec = t3.tv_nsec;
+
+    delay_request.time_sec = t1.tv_sec;
+    delay_request.time_nsec = t1.tv_nsec;
+    mavlink_msg_ptp_timesync_send(
+        _request_sending_link->get_chan(),
+        delay_request.msg_type,
+        delay_request.seq,
+        delay_request.time_sec,
+        delay_request.time_nsec
+        );
     hal.uartA->printf("t1.sec: %d, t1.usec: %d\r\n", t1.tv_sec, t1.tv_nsec);
     hal.uartA->printf("recieved follow up\r\n");
 }
--- libraries/AP_Timesync/AP_Timesync.h
+++ libraries/AP_Timesync/AP_Timesync.h
@@ -4,7 +4,7 @@
 
 #define PTP_SYNC 0x00
 #define PTP_FOLLOW_UP 0x08
-#define PTP_DELAY_REQUSET 0x01
+#define PTP_DELAY_REQUEST 0x01
 #define PTP_DELAY_RESPONSE 0x09
 #define PTP_DEFAULT_STATE 255
 
@@ -20,11 +20,13 @@ class AP_Timesync
         static AP_Timesync *get_singleton() {
             return _singleton;
         }
+        void handle_ptp_timesync(GCS_MAVLINK &link, const mavlink_message_t &msg);
         void handle_sync(mavlink_ptp_timesync_t &packet);
-        void handle_follow_up(mavlink_ptp_timesync_t &packet);
+        void handle_follow_up(class GCS_MAVLINK &, mavlink_ptp_timesync_t &packet);
         void handle_delay_response(mavlink_ptp_timesync_t &packet);        
 
     private:
+        GCS_MAVLINK *_request_sending_link;
         static struct timespec t1, t2, t3, t4;
         static AP_Timesync *_singleton;
 };
@@ -32,3 +34,4 @@ class AP_Timesync
 namespace AP {
     AP_Timesync &ptp();
 };
+
